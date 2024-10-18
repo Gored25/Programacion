b7a35c9a374fea2f90a75113b2294b3a4542251c
Lab5 <Lab5@LAB4-PC11>
2024-10-18 20:59:21 -0300

    Agregamos cedulas por teclado

--- src/pruebaml/PruebaML.java
+++ src/pruebaml/PruebaML.java
@@ -5,12 +5,22 @@ import java.util.*;
 public class PruebaML {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello World");
         ArrayList<Integer> ci = new ArrayList<Integer>();
 
         ci.add(55785144);
 
         System.out.println("Cedula ingresada: " + ci.get(0));
-    }
+        int cedula;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese cedula");
+            cedula = input.nextInt();
+            ci.add(cedula);
+        }
 
+        for (int i = 0; i < ci.size(); i++) {
+            System.out.println("Cedula ingresada: " + ci.get(i));
+        }
+    }
 }
