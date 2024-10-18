a48ee63f71ab12b325ba0e083e28b9f03efc93c6
Lab5 <Lab5@LAB4-PC11>
2024-10-18 20:39:36 -0300

    Subida inicial

---
+++ src/pruebaml/PruebaML.java
@@ -0,0 +1,16 @@
+package pruebaml;
+
+import java.util.*;
+
+public class PruebaML {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        ArrayList<Integer> ci = new ArrayList<Integer>();
+
+        ci.add(55785144);
+
+        System.out.println("Cedula ingresada: " + ci.get(0));
+    }
+
+}
